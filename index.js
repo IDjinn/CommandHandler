6a2f407541c4b42ad0ee23fb56115951d8f608aa
ikubi <mercuryoo@hotmail.com>
2019-08-03 00:05:00 -0300

    Agora é capaz de reconher o nome e o aliases do bot independente se eles estão no objeto info ou qualquer outro objeto do modulo

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const { Client, Collection } = require("discord.js");
-const { readdir, lstatSync } = require("fs");
-const { token, prefix } = require("./config.json");
+const {
+    Client,
+    Collection
+} = require("discord.js");
+const {
+    readdir,
+    lstatSync
+} = require("fs");
+const {
+    token,
+    prefix
+} = require("./config.json");
 const client = new Client();
 
 client.cmds = new Collection();
@@ -18,17 +27,38 @@ const carregarComandos = module.exports.carregarComandos = (dir = "./commands/")
                 if (lstatSync(`./${dir}/${arquivo}`).isDirectory()) {
                     carregarComandos(`./${dir}/${arquivo}`)
                 } else if (arquivo.endsWith(".js")) {
+                    console.log(`Iniciando leitura do arquivo: ${arquivo.split(".")[0]}`)
+                    const salvar = (nome, aliases = [], props) => {
+                        client.cmds.set(nome, props)
+                        if(aliases.length > 0) aliases.forEach((alias) => client.aliases.set(alias, props))
+                        console.log(`Comando salvo: ${nome} | ${aliases.length} aliases`)
+                    }
                     const props = require(`./${dir}/${arquivo}`)
-                    if (!props || !props.info || !props.run || !props.info.aliases || !props.info.name) {
-                        console.log(`Não foi possível carregar o comando ${arquivo.split(".")[0]} pois ou não há ou falta propiedades.`);
+                    if(!props.run)  {
+                        console.log(`Não existe uma função que ative o comando no arquivo: ${arquivo.split(".")[0]}. Então ele foi ignorado`);
                         return;
                     }
-                    client.cmds.set(props.info.name, props);
-                    props.info.aliases.forEach((alias) => {
-                        client.aliases.set(alias, props)
-                    })
 
-                    console.log(`Comando ${props.info.name} e seus ${props.info.aliases.length} aliases salvos.`)
+                    if (props.info && props.info.name) {
+                        const nome = props.info.name
+                        const aliases = props.info.aliases || []
+                        salvar(nome, aliases, props)
+                    } else {
+                        const propsKeys = Object.keys(props)
+                        if (!propsKeys) {
+                            console.log(`Não existem propiedades no arquivo: ${arquivo.split(".")[0]}. Então ele foi ignorado.`)
+                            return;
+                        }
+                        const nomeKey = propsKeys.find((key) => props[key] && (props[key].name || props[key].nome))
+                        if(!nomeKey) {
+                            console.log(`Não existe a propiedade que remeta ao nome do comando no arquivo: ${arquivo.split(".")[0]}. Então ele foi ignorado.`)
+                            return; 
+                        }
+
+                        const nome = props[nomeKey].name || props[nomeKey].nome
+                        const aliases = props[nomeKey].aliases || []
+                        salvar(nome, aliases, props)
+                    }
                 }
             } catch (ex) {
                 console.log(`Erro ao ler o arquivo ${arquivo}`)
@@ -48,19 +78,19 @@ module.exports.run = (client, message, args) => {
 
 module.exports.info = {
     name: "nome do comando",
-    aliases: ["outro meio de chamar o comando"]
+    aliases: ["outro meio de chamar o comando"] -- essa parte é opcional
 }
 */
 
 client.on("message", async message => {
     if (message.author.bot) return;
     if (message.content.indexOf(prefix) !== 0) return;
-    if(message.channel.type != 'text') return; // opcional: vai ignorar todos os comandos que não forem executados em canais de texto
+    if (message.channel.type != 'text') return; // opcional: vai ignorar todos os comandos que não forem executados em canais de texto
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
     const cmdParaExecutar = client.cmds.get(cmd) || client.aliases.get(cmd)
-    if(cmdParaExecutar != null) cmdParaExecutar.run(client, message, args)
+    if (cmdParaExecutar != null) cmdParaExecutar.run(client, message, args)
 })
 
 client.login(token)
